1278c3bebce0ba868d6f3df8dd3a89215fb8f077
Lucija Perkovic <lucijaperkovic@gmail.com>
2022-05-02 04:51:23 +0200

    feat(): backend lists

--- too-doo-api/src/controllers/user-list.controller.ts
+++ too-doo-api/src/controllers/user-list.controller.ts
@@ -1,27 +1,59 @@
 import {authenticate} from '@loopback/authentication';
-import {repository} from '@loopback/repository';
+import {Entity, model, property, repository} from '@loopback/repository';
 import {
-  get, getModelSchemaRef, param
+  get, param
 } from '@loopback/rest';
-import {List} from '../models';
-import {ListRepository} from '../repositories';
+import {Item} from '../models';
+import {ItemRepository, ListRepository} from '../repositories';
 
 @authenticate('jwt')
+
+@model()
+export class ListItemsSchema extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  listId?: string;
+
+  @property({
+    type: 'string',
+    id: true,
+  })
+  userId?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  listName: string;
+
+  @property({
+    type: Array,
+    required: true,
+  })
+  items: Item[];
+  constructor(data?: Partial<ListItemsSchema>) {
+    super(data);
+  }
+}
 
 export class UserListController {
   constructor(
     @repository(ListRepository)
     protected listRepository: ListRepository,
+    @repository(ItemRepository)
+    protected itemRepository: ItemRepository,
   ) { }
 
   @get('/users/{id}/lists', {
     responses: {
       '200': {
-        description: 'Return lists for user',
+        description: 'Token',
         content: {
           'application/json': {
             schema: {
-              type: 'array', lists: getModelSchemaRef(List),
+              type: 'array',
             },
           },
         },
@@ -30,7 +62,27 @@ export class UserListController {
   })
   async find(
     @param.path.string('id') id: string,
-  ): Promise<List[]> {
-    return this.listRepository.find({where: {userId: id}})
+  ): Promise<{lists: ListItemsSchema[]}> {
+    const listsR = await this.listRepository.find({where: {userId: id}});
+    let items: Item[];
+    let lists: ListItemsSchema[] = [];
+    await Promise.all(listsR.map(async (list) => {
+      const itemsL = await this.itemRepository.find({where: {listId: list.uuid}});
+      items = itemsL.map((item: any) => {
+        return item;
+      });
+      const listItems = new ListItemsSchema(
+        {
+          listId: list.uuid,
+          userId: list.userId,
+          listName: list.name,
+          items: items
+        }
+      )
+      lists.push(listItems)
+      return true
+    }))
+    console.log({lists})
+    return {lists}
   }
 }
